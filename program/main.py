a086f7b5bd05b349f55844da8bebe823a02e0186
Josh Feinberg <feinber.josh@gmail.com>
2023-02-22 09:37:00 -0600

    place order

--- program/main.py
+++ program/main.py
@@ -3,6 +3,7 @@ from func_connections import connect_dydx
 from func_private import ABORT_ALL_POSITIONS
 from func_public import construct_market_prices
 from func_cointegration import store_cointegration_results
+from func_entry_pairs import open_positions
 
 if __name__ == "__main__":
     
